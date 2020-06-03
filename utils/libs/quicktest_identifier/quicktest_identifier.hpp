ea70bd0b9da28875a98b91185cd1c24dcf8b1b41
Mihir Luthra <1999mihir.luthra@gmail.com>
2020-06-03 18:43:30 +0530

    common valid identifier detector header

---
+++ utils/libs/quicktest_identifier/quicktest_identifier.hpp
@@ -0,0 +1,87 @@
+#ifndef __QUICKTEST_IDENTIFIER_HPP__
+#define __QUICKTEST_IDENTIFIER_HPP__
+
+#include <cctype>
+#include <cstring>
+#include <string>
+
+/*
+ * identifier validity is checked at various places,
+ * like checked key in qtc file, checking qtc func name
+ * etc.
+ * This file defines the rules for its validity.
+ */
+
+namespace quicktest_identifier {
+	bool is_invalid(char ch)
+	{
+		return (!std::isalnum(ch) && ch != '_' && ch != '-');
+	}
+
+	bool is_invalid(std::string str)
+	{
+		if (str.empty()) {
+			return true;
+		}
+
+		for (auto &c : str) {
+			if (is_invalid(c)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	bool is_invalid(const char *str)
+	{
+		if (!std::strcmp(str, "")) {
+			return true;
+		}
+
+		for (int i = 0 ; str[i] != '\0' ; ++i) {
+			if (is_invalid(str[i])) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	bool is_valid(char ch)
+	{
+		return (std::isalnum(ch) || ch == '_' || ch == '-');
+	}
+
+	bool is_valid(std::string str)
+	{
+		if (str.empty()) {
+			return false;
+		}
+
+		for (auto &c : str) {
+			if (is_invalid(c)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	bool is_valid(const char *str)
+	{
+		if (!std::strcmp(str, "")) {
+			return false;
+		}
+
+		for (int i = 0 ; str[i] != '\0' ; ++i) {
+			if (is_invalid(str[i])) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
+
+#endif /* __QUICKTEST_IDENTIFIER_HPP__ */
