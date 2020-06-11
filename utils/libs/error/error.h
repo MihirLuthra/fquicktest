9ddbb96099ba54fa56560c9e02df3e8af4962f4e
Mihir Luthra <1999mihir.luthra@gmail.com>
2020-06-11 10:06:28 +0530

    common error handling file

---
+++ utils/libs/error/error.h
@@ -0,0 +1,23 @@
+#ifndef __ERROR_H__
+#define __ERROR_H__
+
+#ifdef __cplusplus
+#	include <cstdio>
+#else
+#	include <stdio.h>
+#endif
+
+#define RED         "\033[31m"
+#define BRIGHT_RED  "\033[91m"
+#define RESET_COLOR "\033[0m"
+
+#define BOLD_ON  "[1m"
+#define BOLD_OFF "[0m"
+
+#define P_ERR(fmt, ...) \
+	fprintf(stderr, BOLD_ON RED "%s: Line %d: " BOLD_OFF BRIGHT_RED fmt "\n" RESET_COLOR, __FILE__, __LINE__, __VA_ARGS__)
+
+#define P_ERR_NA(fmt) \
+	fprintf(stderr, BOLD_ON RED "%s: Line %d: " BOLD_OFF BRIGHT_RED fmt "\n" RESET_COLOR, __FILE__, __LINE__)
+
+#endif /* __ERROR_H__ */
